68dd57a26d5ba0962c9cc36c7a961617ecc91302
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-12 17:37:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz val
-  if val % 3 == 0
+  if val % 3 == 0 && val % 5 != 0
     "Fizz"
-  elsif val % 5 == 0
+  elsif val % 5 == 0 && val % 3 != 0
     "Buzz"
-    elsif val % 3 == 0 || val % 5 == 0
+    elsif val % 3 == 0 && val % 5 == 0
       "FizzBuzz"
     else
       nil
